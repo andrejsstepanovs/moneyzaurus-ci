ca53b10b4e60a5309884615c3cb46a57b8989a35
Andrejs Stepanovs <andrejsstepanovs@gmail.com>
2016-01-01 16:13:54 +0100

    include new js script

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -9,7 +9,8 @@
 		<link rel="stylesheet" href="<?php echo WEB_BASE_URL; ?>/media/css/lib/awesomplete.css">
 		<link rel="stylesheet" href="<?php echo WEB_BASE_URL; ?>/media/css/site.css">
 		<script src="<?php echo WEB_BASE_URL; ?>/media/js/lib/jquery.min.js" type="text/javascript"></script>
-		<script src="<?php echo WEB_BASE_URL; ?>/media/js/lib/awesomplete.js" type="text/javascript" async></script>
+        <script src="<?php echo WEB_BASE_URL; ?>/media/js/site.js" type="text/javascript"></script>
+        <script src="<?php echo WEB_BASE_URL; ?>/media/js/lib/awesomplete.js" type="text/javascript" async></script>
     </head>
 	<body>
 		<div class="body">
@@ -20,8 +21,9 @@
 					<ul class="pure-menu-list">
 						<li class="pure-menu-item"><a href="/transaction" class="pure-menu-link">New</a></li>
 						<li class="pure-menu-item"><a href="/data" class="pure-menu-link">Data</a></li>
-						<li class="pure-menu-item"><a href="/chart/pie" class="pure-menu-link">Pie</a></li>
-						<li class="pure-menu-item"><a href="/chart" class="pure-menu-link">Chart</a></li>
+<!--                        <li class="pure-menu-item"><a href="/manager" class="pure-menu-link">Manager</a></li>-->
+                        <li class="pure-menu-item"><a href="/chart/pie" class="pure-menu-link">Pie</a></li>
+                        <li class="pure-menu-item"><a href="/chart" class="pure-menu-link">Chart</a></li>
 						<li class="pure-menu-item"><a href="/profile" class="pure-menu-link">Profile</a></li>
 					</ul>
 				</div>
